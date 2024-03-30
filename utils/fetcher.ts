7a3a674ab3dbf7de94adcc32bbc49d3257aee7cf
Lorenzo <hello@lorenzo0111.me>
2024-03-30 16:14:23 +0100

    Fixed build

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -2,5 +2,5 @@ import { SWRConfiguration } from "swr";
 import useSWR from "swr/immutable";
 
 export const fetcher = (url: string) => fetch(url).then((res) => res.json());
-export const useFetcher = <T>(url: string | null, options?: SWRConfiguration) =>
+export const useFetcher = <T = any>(url: string | null, options?: SWRConfiguration) =>
   useSWR<T>(url, fetcher, options);
