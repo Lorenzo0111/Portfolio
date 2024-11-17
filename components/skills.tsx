9dd79235a61423ed13f48c1e30f4527fa2a08e27
Lorenzo <hello@lorenzo0111.me>
2024-11-17 17:58:02 +0100

    Added new skills

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,8 +1,10 @@
 import {
   Box,
   ChevronsLeftRightEllipsis,
+  Database,
   HardDrive,
   Paintbrush,
+  Smartphone
 } from "lucide-react";
 import Skill from "./skill";
 
@@ -13,16 +15,22 @@ export default function Skills() {
 
       <div className="flex flex-wrap gap-4 justify-center 2xl:justify-between">
         <Skill skills={{ Spigot: 100, Paper: 75, NMS: 65 }}>
-          <Box className="mr-2" /> Plugin Developer
+          <Box className="mr-2" /> Plugin Development
         </Skill>
-        <Skill skills={{ "Node.js": 90, React: 90, Vue: 75 }}>
-          <ChevronsLeftRightEllipsis className="mr-2" /> Fullstack Developer
+        <Skill skills={{ "Node.js": 90, "React / Next.js": 90, Vue: 75 }}>
+          <ChevronsLeftRightEllipsis className="mr-2" /> Fullstack Development
         </Skill>
-        <Skill skills={{ Ubuntu: 85, Windows: 70 }}>
-          <HardDrive className="mr-2" /> System Administrator
+        <Skill skills={{ Docker: 90, Debian: 85 }}>
+          <HardDrive className="mr-2" /> System Administration
         </Skill>
         <Skill skills={{ UIX: 85, "Logo Design": 75 }}>
-          <Paintbrush className="mr-2" /> Designer
+          <Paintbrush className="mr-2" /> Design
+        </Skill>
+        <Skill skills={{ Capacitor: 90, "React Native": 75 }}>
+          <Smartphone className="mr-2" /> Mobile Development
+        </Skill>
+        <Skill skills={{ MySQL: 85, MongoDB: 75, Postgres: 75 }}>
+          <Database className="mr-2" /> Databases
         </Skill>
       </div>
     </div>
