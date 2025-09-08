bf43927423c7474fd593169d8e5fb6ecc84589a0
Lorenzo <hello@lorenzo0111.me>
2025-09-08 18:49:09 +0200

    Fixed suspense

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,6 @@
 import SignIn from "@/components/auth/SignIn";
 import type { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Login",
@@ -9,7 +10,9 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <main className="mx-auto min-h-[70vh] flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-yellow-100/40 via-transparent to-transparent">
-      <SignIn />
+      <Suspense>
+        <SignIn />
+      </Suspense>
     </main>
   );
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,6 @@
 import SignUp from "@/components/auth/SignUp";
 import type { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Register",
@@ -9,7 +10,9 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <main className="mx-auto min-h-[70vh] flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-yellow-100/40 via-transparent to-transparent">
-      <SignUp />
+      <Suspense>
+        <SignUp />
+      </Suspense>
     </main>
   );
 }
